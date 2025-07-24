666dc36665ca2332f9d8e3c87caffc7203534c21
Jyrki Launonen <jyrki.launonen@gmail.com>
2025-07-24 21:36:28 +0300

    Wrap time md variables in a time tag.
    Also fix date format not using localtime conversion for datetimes.

--- kirppu/views/ui_text_utils.py
+++ kirppu/views/ui_text_utils.py
@@ -1,4 +1,5 @@
 import datetime
+import html
 import typing
 
 from django.conf import settings
@@ -14,24 +15,32 @@ def _convert_localtime(value: datetime.datetime) -> datetime.datetime:
     return value
 
 
+def _wrap(dt, value) -> str:
+    return f'<time datetime="{html.escape(dt)}">{html.escape(value, quote=False)}</time>'
+
+
 def _datetime(value: datetime.datetime | None) -> Lazy:
     def wrapped(fmt: str = "SHORT_DATETIME_FORMAT") -> str:
         if value is None:
             return ""
         local_value = _convert_localtime(value)
-        return defaultfilters.date(local_value, fmt)
+        dt = local_value.isoformat(timespec="minutes")
+        return _wrap(dt, defaultfilters.date(local_value, fmt))
 
     return wrapped
 
 
-def _date(value: datetime.datetime | None) -> Lazy:
+def _date(value: datetime.date | None) -> Lazy:
     def wrapped(fmt: str = "SHORT_DATE_FORMAT") -> str:
         if value is None:
             return ""
-        if isinstance(value, datetime.date):
-            return defaultfilters.date(value, fmt)
-        local_value = _convert_localtime(value)
-        return defaultfilters.date(local_value.date(), fmt)
+        # datetime is a date.
+        if isinstance(value, datetime.datetime):
+            local_value = _convert_localtime(value)
+            date = local_value.date()
+            return _wrap(date.isoformat(), defaultfilters.date(date, fmt))
+        else:
+            return _wrap(value.isoformat(), defaultfilters.date(value, fmt))
 
     return wrapped
 
@@ -41,7 +50,8 @@ def _time(value: datetime.datetime | None) -> Lazy:
         if value is None:
             return ""
         local_value = _convert_localtime(value)
-        return defaultfilters.time(local_value.time(), fmt)
+        time = local_value.time()
+        return _wrap(time.isoformat(timespec="minutes"), defaultfilters.time(time, fmt))
 
     return wrapped
 
