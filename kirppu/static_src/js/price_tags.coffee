jlaunonen/kirppu

commit 3b14e52e7ab4b15e8419095700c5344ac6948da1

Add maxlength limit to item inline edits.

kirppu/static_src/js/price_tags.coffee
@@ -18,6 +18,7 @@ class PriceTagsConfig
   enabled: true
   price_min: 0
   price_max: 400
+  name_max_len: 50
 
   constructor: ->
 
@@ -192,7 +193,7 @@ spinner = (from, display) ->
 # @param classname [String] Field classname.
 # @param placeholder [String] Placeholder text for the input field.
 # @param callback [function?] Optional callback, called with .item_container and value returned.
-bindEditable = (urlFn, classname, placeholder, callback) ->
+bindEditable = (urlFn, classname, placeholder, callback, max_len = 50) ->
   new Malle.Malle(
     tooltip: gettext("Click to edit...")
     placeholder: placeholder
@@ -208,6 +209,9 @@ bindEditable = (urlFn, classname, placeholder, callback) ->
     # There seems to be a bug in Malle's internal diff behavior where second blur leaves the form open.
     # Ignore it by always entering in fun and check the diff by ourselves.
     requireDiff: false
+
+    onEdit: (original, event, input) ->
+      input.maxLength = max_len
 
     fun: (value, original, event, input) ->
       code = getCode(input)
@@ -371,7 +375,7 @@ bindItemToggleEvents = (tag, code) ->
 # @param tags [String] A set of '.item_container' elements.
 bindTagEvents = (tags) ->
   if C.enabled
-    bindEditable(C.name_update_url, "item_name", gettext("Name"))
+    bindEditable(C.name_update_url, "item_name", gettext("Name"), C.name_max_len)
     bindEditable(C.price_update_url, "item_price", gettext("Price"), (tag, value) ->
       $(".item_head_price", tag).text(value)
     )

kirppu/templates/kirppu/app_items.html
@@ -153,6 +153,7 @@
         itemsConfig.urls.all_to_print = "{% url 'kirppu:all_to_print' event_slug=event.slug %}";
         itemsConfig.urls.item_to_print = "{% url 'kirppu:item_to_not_printed' event_slug=event.slug code='__placeholder__' %}";
         itemsConfig.urls.item_hide = "{% url 'kirppu:item_hide' event_slug=event.slug code='__placeholder__' %}";
+        itemsConfig.name_max_len = {{ name_max_len }};
         {% if not is_registration_open %}itemsConfig.enabled = false;{% endif %}
         {% if PRICE_MIN_MAX %}
         itemsConfig.price_min = {{ PRICE_MIN_MAX.0 }};

kirppu/views/monolithic.py
@@ -471,6 +471,7 @@ def get_items(request, event_slug, bar_type):
         'itemTypes': ItemType.as_tuple(event),
         'CURRENCY': settings.KIRPPU_CURRENCY,
         'PRICE_MIN_MAX': settings.KIRPPU_MIN_MAX_PRICE,
+        'name_max_len': Item._meta.get_field('name').max_length,
     }
     render_params.update(vendor_data)
 

commit 8b6ad5972148b93a0446503b3264bd39a56fba94

Fix inline item name changing.
Broken in 3b14e52e7ab4b15e8419095700c5344ac6948da1.

kirppu/static_src/js/price_tags.coffee
@@ -375,10 +375,10 @@ bindItemToggleEvents = (tag, code) ->
 # @param tags [String] A set of '.item_container' elements.
 bindTagEvents = (tags) ->
   if C.enabled
-    bindEditable(C.name_update_url, "item_name", gettext("Name"), C.name_max_len)
+    bindEditable(C.name_update_url, "item_name", gettext("Name"), null, C.name_max_len)
     bindEditable(C.price_update_url, "item_price", gettext("Price"), (tag, value) ->
       $(".item_head_price", tag).text(value)
-    )
+    , 10)
   else
     $(tags).removeClass("item_editable")
 