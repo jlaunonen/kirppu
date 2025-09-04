jlaunonen/kirppu

commit 9dcfdc0fcc4f30c9016bd214ae0b711b3e262da5

Replace jquery.jeditable with Malle.
- Change vendor apis to use plain text responses as they are not really
html which is the default in HttpResponse.

kirppu/static_src/js/price_tags.coffee
@@ -6,7 +6,6 @@ class PriceTagsConfig
     code: ''
 
   urls:
-    roller: ''
     name_update: ''
     price_update: ''
     item_to_list: ''
@@ -22,11 +21,11 @@ class PriceTagsConfig
 
   constructor: ->
 
-  name_update_url: (code) ->
+  name_update_url: (code) =>
     url = @urls.name_update
     return url.replace(@url_args.code, code)
 
-  price_update_url: (code) ->
+  price_update_url: (code) =>
     url = @urls.price_update
     return url.replace(@url_args.code, code)
 
@@ -66,7 +65,7 @@ createTag = (name, price, vendor_id, code, dataurl, type, adult) ->
 
   $('.item_vendor_id', tag).text(vendor_id)
 
-  $(tag).attr('id', code)
+  $(tag).attr('id', code).data("code", code)
   $('.item_extra_code', tag).text(code)
 
   $('.barcode_container > img', tag).attr('src', dataurl)
@@ -174,35 +173,67 @@ bindFormEvents = ->
   return
 
 
-# Bind events for item price editing.
-# @param tag [jQuery element] An '.item_container' element.
-# @param code [String] Barcode string of the item.
-bindPriceEditEvents = (tag, code) ->
-  $(".item_price", tag).editable(
-    C.price_update_url(code),
-    indicator: "<img src='" + C.urls.roller + "'>"
-    tooltip:   gettext("Click to edit...")
-    placeholder: gettext("<em>Click to edit</em>")
-    onblur:    "submit"
-    style:     "width: 2cm"
-    # Update the extra price display for long tags.
-    callback:  (value) -> $(".item_head_price", tag).text(value)
-  )
-  return
+# Get barcode from any child of the .item_container.
+getCode = (from) ->
+  return $(from).parents("[data-code]").data("code")
 
 
-# Bind events for item name editing.
-# @param tag [jQuery element] An '.item_container' element.
-# @param code [String] Barcode string of the item.
-bindNameEditEvents = (tag, code) ->
-  $(".item_name", tag).editable(
-    C.name_update_url(code),
-    indicator: "<img src='" + C.urls.roller + "'>"
-    tooltip:   gettext("Click to edit...")
-    placeholder: gettext("<em>Click to edit</em>")
-    onblur:    "submit"
-    style:     "inherit"
-  )
+# Show or hide progress spinner.
+spinner = (from, display) ->
+  s = $(from).parents(".item_container").children(".spinner")
+  if display
+    s.show()
+  else
+    s.hide(400)
+
+
+# Bind events for editing a field.
+# @param urlFn [function] Function returning url for updating this field.
+# @param classname [String] Field classname.
+# @param placeholder [String] Placeholder text for the input field.
+# @param callback [function?] Optional callback, called with .item_container and value returned.
+bindEditable = (urlFn, classname, placeholder, callback) ->
+  new Malle.Malle(
+    tooltip: gettext("Click to edit...")
+    placeholder: placeholder
+
+    listenOn: ".item_editable ." + classname
+    listenNow: true
+
+    formClasses: [classname]
+
+    # Debug note: Changing this to Ignore may help inspecting the form in browser.
+    onBlur: Malle.Action.Submit
+
+    # There seems to be a bug in Malle's internal diff behavior where second blur leaves the form open.
+    # Ignore it by always entering in fun and check the diff by ourselves.
+    requireDiff: false
+
+    fun: (value, original, event, input) ->
+      code = getCode(input)
+      oval = $(original).text()
+
+      if oval == value or not value
+        # Don't submit equal or empty value.
+        return Promise.resolve(oval)
+
+      spinner(input, true)
+      return $.post(
+        url: urlFn(code)
+        data:
+          value: value
+      ).always(() ->
+        spinner(input, false)
+      ).then((rval) ->
+        if callback?
+          p = $(input).parents(".item_container").first()
+          callback(p, rval)
+        rval
+      , (e) ->
+        console.log(e.responseText)
+        oval
+      )
+    )
   return
 
 
@@ -337,17 +368,20 @@ bindItemToggleEvents = (tag, code) ->
 
 
 # Bind events for a set of '.item_container' elements.
-# @param tags [jQuery set] A set of '.item_container' elements.
+# @param tags [String] A set of '.item_container' elements.
 bindTagEvents = (tags) ->
-  tags.each((index, tag) ->
+  if C.enabled
+    bindEditable(C.name_update_url, "item_name", gettext("Name"))
+    bindEditable(C.price_update_url, "item_price", gettext("Price"), (tag, value) ->
+      $(".item_head_price", tag).text(value)
+    )
+  else
+    $(tags).removeClass("item_editable")
+
+  $(tags).each((index, tag) ->
     tag = $(tag)
     code = tag.attr('id')
 
-    if C.enabled
-      bindPriceEditEvents(tag, code)
-      bindNameEditEvents(tag, code)
-    else
-      tag.removeClass("item_editable")
     bindItemHideEvents(tag, code)
     bindItemToPrintedEvents(tag, code)
     bindItemToggleEvents(tag, code)

kirppu/templates/kirppu/app_items.html
@@ -5,7 +5,7 @@
 {% block head %}
     {{ block.super }}
     <link href="{% static "kirppu/css/price_tags.css" %}" rel="stylesheet">
-    <script src="{% static "kirppu/js/jeditable.js" %}"></script>
+    <script src="{% static "kirppu/js/malle.js" %}"></script>
     <script src="{% static "kirppu/js/price_tags.js" %}"></script>
     <style type="text/css">
         {% include "kirppu/currency_style.css" %}
@@ -151,7 +151,6 @@
         itemsConfig.urls.item_to_list = "{% url 'kirppu:item_to_printed' event_slug=event.slug code='__placeholder__' %}";
         itemsConfig.urls.size_update = "{% url 'kirppu:item_update_type' event_slug=event.slug code='__placeholder__' %}";
         itemsConfig.urls.all_to_print = "{% url 'kirppu:all_to_print' event_slug=event.slug %}";
-        itemsConfig.urls.roller = "{% static "kirppu/img/roller.gif" %}";
         itemsConfig.urls.item_to_print = "{% url 'kirppu:item_to_not_printed' event_slug=event.slug code='__placeholder__' %}";
         itemsConfig.urls.item_hide = "{% url 'kirppu:item_hide' event_slug=event.slug code='__placeholder__' %}";
         {% if not is_registration_open %}itemsConfig.enabled = false;{% endif %}
@@ -165,7 +164,7 @@
 
             bindFormEvents();
 
-            bindTagEvents($('#items > .item_editable'));
+            bindTagEvents('#items > .item_editable');
             bindListTagEvents($('#printed_items > .item_container'));
         });
     </script>

kirppu/gulpfile.js
@@ -105,9 +105,10 @@ const cssTasks = Object.entries(pipeline.css).map(function([name, def]) {
 
 const rollupTasks = Object.entries(pipeline.rollup).map(function([name, def]) {
     const taskName = "rollup:" + name;
+    const input = def.source_filename ? srcPrepend(def.source_filename) : srcPrepend(def);
     gulp.task(taskName, function() {
         return rollup.rollup({
-            input: srcPrepend(def.source_filename),
+            input: input,
             plugins: [
                 rollup_sucrase({
                     jsxPragma: 'redom.el',

kirppu/pipeline.js
@@ -48,7 +48,7 @@ module.exports.js = {
             "../node_modules/bootstrap/dist/js/bootstrap.js",
             "js/csrf.coffee",
             "js/customtexts_front.js",
-            "../node_modules/redom/dist/redom.js"
+            "../node_modules/redom/dist/redom.js",
         ],
         "output_filename": "general.js",
         "compress": true,
@@ -128,13 +128,6 @@ module.exports.js = {
         "output_filename": "checkout_comp.js",
         "compress": true
     },
-    'jeditable': {
-        "source_filenames": [
-            "js/jquery.jeditable.js",
-        ],
-        "output_filename": "jeditable.js",
-        "compress": true,
-    },
     'command_list': {
         "source_filenames": [
             "js/commands.coffee"
@@ -182,7 +175,12 @@ module.exports.rollup = {
             "jst/*.jsx"
         ],
         "output_name": "Template"
-    }
+    },
+    "malle": {
+        "output_filename": "js/malle.js",
+        "source_filename": "../node_modules/@deltablot/malle/dist/main.js",
+        "output_name": "Malle",
+    },
 };
 
 module.exports.static = {

kirppu/static_src/css/price_tags.css
@@ -1,3 +1,11 @@
+html, :root {
+    --input-focus-border-color: #00aaff;
+    --input-border-color: #44a;
+    --input-border-size: 2px;
+    --input-border-neg-size: -2px;
+    --input-border: var(--input-border-size) solid;
+}
+
 #item-add-form, #item-add-controls {
     width: 19cm;
 }
@@ -91,16 +99,18 @@
 }
 
 .item_price {
+    display: inline-block;
     width: 2.5cm;
     font-size: 150%;
-    margin-top: 3px;
-    margin: 2px; /* Compensate for border not being there for locked items */
+    margin-left: var(--input-border-size);
+    margin-top: var(--input-border-neg-size);
 }
 
 @media screen, projection, handheld {
     .item_editable .item_price {
-        margin: 0px;
-        border: 2px inset;
+        margin-left: 0;
+        height: 1.6em;
+        border: var(--input-border) var(--input-border-color);
     }
 }
 
@@ -115,14 +125,28 @@
 }
 
 /* Price tag edit forms. */
-.item_editable .item_body form {
-    display: inline;
+.item_editable .item_body form:has(input:focus) {
+    border: var(--input-border) var(--input-focus-border-color);
+    position: relative;
+    z-index: 100;
 }
 .item_editable .item_body form input {
     background: transparent;
     margin: 0;
     padding: 0;
     border: none;
+    outline: none;
+    width: 100%;
+    min-height: 0;
+}
+.item_container {
+    position: relative;
+}
+.item_container img.spinner {
+    display: inline-block;
+    position: absolute;
+    left: 46%;
+    top: 8px;
 }
 
 .item_head_price {
@@ -150,8 +174,8 @@
 
 @media screen, projection, handheld {
     .item_editable .item_name {
-        margin: 0px;
-        border: 2px inset;
+        margin: 0;
+        border: var(--input-border) var(--input-border-color);
     }
 }
 

kirppu/templates/kirppu/app_boxes.html
@@ -5,7 +5,6 @@
 {% block head %}
     {{ block.super }}
     <link href="{% static "kirppu/css/boxes.css" %}" rel="stylesheet">
-    <script src="{% static "kirppu/js/jeditable.js" %}"></script>
     <script src="{% static "kirppu/js/boxes.js" %}"></script>
     <style type="text/css">
         {% include "kirppu/currency_style.css" with class="box_price" %}

kirppu/templates/kirppu/app_boxes_content.html
@@ -8,7 +8,6 @@
     <link href="{% static "kirppu/css/boxes.css" %}" rel="stylesheet">
     <link href="{% static "kirppu/css/price_tags.css" %}" rel="stylesheet">
     <script src="{% static "kirppu/js/general.js" %}"></script>
-    <script src="{% static "kirppu/js/jeditable.js" %}"></script>
     <script src="{% static "kirppu/js/boxes.js" %}"></script>
     <style type="text/css">
         {% include "kirppu/currency_style.css" %}

commit cadea3da042cdcf6f96be3cd80a6871e452b9a1e

Fix nonworking toggle buttons in price tags.
The class required for toggles to work is removed when registration
ends, or the item is locked.

Broken likely in 9dcfdc0fcc4f30c9016bd214ae0b711b3e262da5.

kirppu/models.py
@@ -1075,6 +1075,7 @@ class Item(models.Model):
         "name",
         "type",
         "adult",
+        "is_locked",
         price=lambda self: str(self.price_fmt).replace(".", ","),
     )
 

kirppu/static_src/js/price_tags.coffee
@@ -270,6 +270,8 @@ moveItemToNotPrinted = (tag, code) ->
       $(new_tag).hide()
       $(new_tag).appendTo("#items")
       $(new_tag).show('slow')
+      if item.is_locked
+        new_tag.removeClass("item_editable")
       bindTagEvents($(new_tag))
 
     error: (item) ->

kirppu/templates/kirppu/app_items.html
@@ -165,7 +165,7 @@
 
             bindFormEvents();
 
-            bindTagEvents('#items > .item_editable');
+            bindTagEvents('#items > .item_container');
             bindListTagEvents($('#printed_items > .item_container'));
         });
     </script>