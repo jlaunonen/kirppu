jlaunonen/kirppu

commit 89a4838a1f7a1557bfa3ed9e3bcd1e55c15203cf

Refactor Dialog to use JSX template.
Instead of using static template, use dynamic one to prevent
accidentally clearing the contents on hide when two dialogs are shown
subsequently.

Add Dialog2 for simpler alternative.

kirppu/static_src/js/checkout/dialog.coffee
@@ -1,8 +1,15 @@
 # Dialog helper class for using BootStrap Modal Dialog with only one common template.
 class @Dialog
-  constructor: (template="#dialog_template", title="#dialog_template_label") ->
+  constructor: () ->
+    template = Template.dialog(
+      title: "??"
+      body: ""
+      buttons: []
+    )
+    @_dom = $("#dialog_template")
+
     @container = $(template)
-    @title = @container.find(title)
+    @title = @container.find("#dialog_label")
     @body = @container.find(".modal-body")
     @buttons = @container.find(".modal-footer")
 
@@ -12,9 +19,7 @@ class @Dialog
 
     @_buttonList = []
     @container.on("hidden.bs.modal", () =>
-      @title.empty()
-      @body.empty()
-      @buttons.empty()
+      @container.remove()
     )
 
   # Add positive button to the dialog.
@@ -64,8 +69,23 @@ class @Dialog
   # Display the dialog. This will append added buttons to `buttons`-container.
   # @param modalArgs [optional] Arguments for BootStrap `modal()`.
   show: (modalArgs=keyboard:false) ->
+    @_dom.append(@container)
     @buttons.append(@_buttonList)
     @container.modal(modalArgs)
 
   _button: (clazz="default") ->
     $("""<button type="button" class="btn btn-#{ clazz }" data-dismiss="modal">""")
+
+
+class @Dialog2
+  constructor: (obj) ->
+    @_dom = $("#dialog_template")
+
+    @_dialog = $(Template.dialog(obj))
+    @_dialog.on("hidden.bs.modal", () =>
+      @_dialog.remove()
+    )
+
+  show: (modalArgs=keyboard: false) ->
+    @_dom.append(@container)
+    @_dialog.modal(modalArgs)

kirppu/static_src/js/checkout/itemcheckinmode.coffee
@@ -74,8 +74,6 @@ class @ItemCheckInMode extends ItemCheckoutMode
 
   _boxDialog: (data) =>
     # Accepted, but not done.
-    dlg = new Dialog()
-    dlg.title.text(gettext("Mark the box number"))
     bundle_size = data.box.bundle_size
     body = $ Template.box_check_in_dialog(
       item: data
@@ -89,16 +87,21 @@ class @ItemCheckInMode extends ItemCheckoutMode
         bundle_size: if bundle_size > 1 then ngettext("%i pc", "%i pcs", bundle_size).replace("%i", bundle_size) \
           else ""
     )
-    dlg.body.append(body)
 
-    dlg.addNegative().text(gettext("Cancel"))
-    dlg.addPositive().text(gettext("Accept")).click(() =>
-      Api.box_checkin(
-        code: data.code
-        box_info: data.box.box_number
-      ).then(
-        (data2, _, jqXHR) => @_onAddItem(data2, jqXHR.status, true)
-      , @onResultError)
+    dlg = new Dialog2(
+      titleText: gettext("Mark the box number")
+      body: body
+      buttons: [
+        {text: gettext("Cancel"), classes: "btn-default"},
+        {text: gettext("Accept"), classes: "btn-primary", click: =>
+          Api.box_checkin(
+            code: data.code
+            box_info: data.box.box_number
+          ).then(
+            (data2, _, jqXHR) => @_onAddItem(data2, jqXHR.status, true)
+          , @onResultError)
+        }
+      ]
     )
 
     dlg.show()

kirppu/static_src/jst/dialog.jsx
@@ -1,0 +1,40 @@
+function btn({text, classes, dismiss, click}) {
+    if (dismiss === undefined || dismiss === null) {
+        dismiss = true
+    }
+    return <button type="button" className={"btn " + classes} data-dismiss={dismiss ? "modal" : ""} onclick={click}>{text}</button>
+}
+
+/**
+ * @typedef DialogButton
+ * @type {object}
+ * @property {String} text
+ * @property {String} classes
+ * @property {Boolean} [dismiss]
+ * @property {Function, null} [click]
+ */
+
+/**
+ * @param {Object} params
+ * @param {String} params.titleText
+ * @param {DialogButton[]} params.buttons
+ */
+export default function render({titleText, body, buttons}) {
+    return (
+        <div className="modal fade" tabIndex="-1" role="dialog" aria-labelledby="dialog_label">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <button type="button" className="close" data-dismiss="modal"><span
+                            aria-hidden="true">&times;</span><span className="sr-only">{gettext("Close")}</span></button>
+                        <h4 className="modal-title" id="dialog_label">{titleText}</h4>
+                    </div>
+                    <div className="modal-body">{body}</div>
+                    <div className="modal-footer">
+                        {buttons.map((e) => btn(e))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}

kirppu/static_src/jst/index.js
@@ -2,6 +2,7 @@ import box_check_in_dialog from "./box_check_in_dialog.jsx";
 import box_report_table from "./box_report_table.jsx";
 import box_sell_allocation_dialog from "./box_sell_allocation_dialog.jsx";
 import counter_list from "./counter_list.jsx";
+import dialog from "./dialog.jsx";
 import {item_edit_dialog_content, item_edit_dialog_modal} from './item_edit_dialog.jsx';
 import {item_find_table, item_find_table_item, item_find_table_no_results} from "./item_find_table.jsx";
 import {item_receipt_table, item_receipt_table_row} from './item_receipt_table.jsx';
@@ -27,6 +28,7 @@ const Template = {
     box_report_table: box_report_table,
     box_sell_allocation_dialog: box_sell_allocation_dialog,
     counter_list: counter_list,
+    dialog: dialog,
     item_edit_dialog_content: item_edit_dialog_content,
     item_edit_dialog_modal: item_edit_dialog_modal,
     item_find_table: item_find_table,

kirppu/templates/kirppu/dialog_template.html
@@ -1,17 +1,2 @@
-<div class="modal fade" id="dialog_template" tabindex="-1" role="dialog" aria-labelledby="dialog_template_label"
-         aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal"><span
-                        aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                <h4 class="modal-title" id="dialog_template_label">&lt;Dialog&gt;</h4>
-            </div>
-            <div class="modal-body">
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
-</div>
+{# placeholder for Dialog.coffee #}
+<div id="dialog_template"></div>

commit 3b7a79d56be538907bcc94d4412ada11a0203ec8

checkout: Fix dialog autofocus regression
The ModeSwitcher adds an event listener to #dialog_template / @_dom, but
Dialog2 did not add the dialog into it as it referred to wrong
attribute. Thus when the dialog was closed, it never caused the main
input to regain focus.

Likely broken in 89a4838a1f7a1557bfa3ed9e3bcd1e55c15203cf

kirppu/static_src/js/checkout/dialog.coffee
@@ -93,7 +93,7 @@ class @Dialog2
     )
 
   show: (modalArgs=keyboard: false) ->
-    @_dom.append(@container)
+    @_dom.append(@_dialog)
     @_dialog.modal(modalArgs)
 
   hide: () ->