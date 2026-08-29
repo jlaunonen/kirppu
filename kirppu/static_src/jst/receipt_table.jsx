jlaunonen/kirppu

commit b5b8eb0b015421bc8b36408135d8c42604b1c47a

Convert more templates to JSX.

kirppu/static_src/jst/receipt_table.jsx
@@ -1,0 +1,32 @@
+// In print receipt.
+
+export function receipt_table({caption, vendor, code, item, price}) {
+    return (
+        <table className="table table-striped table-hover table-condensed">
+            {caption && <caption className="h3">{caption}</caption>}
+            <thead>
+            <tr>
+                <th className="receipt_vendor_id">{vendor}</th>
+                <th className="receipt_code">{code}</th>
+                <th className="receipt_item">{item}</th>
+                <th className="receipt_price">{price}</th>
+            </tr>
+            </thead>
+            <tbody/>
+        </table>
+    )
+}
+
+export function receipt_table_row({joined, vendor, code, name, price, text}) {
+    if (joined) {
+        return <td colSpan="4">{text}</td>
+    }
+    return (
+        <tr id={code}>
+            <td className="receipt_vendor_id numeric">{vendor}</td>
+            <td className="receipt_code">{code}</td>
+            <td className="receipt_item">{name}</td>
+            <td className="receipt_price numeric">{price}</td>
+        </tr>
+    )
+}

kirppu/pipeline.js
@@ -159,19 +159,6 @@ module.exports.js = {
 module.exports.jst = {
     "checkout_templates": {
         "source_filenames": [
-            "jst/item_report_table.jinja2",
-            "jst/box_report_table.jinja2",
-            "jst/box_sell_allocation_dialog.jinja2",
-            "jst/item_receipt_table.jinja2",
-            "jst/item_receipt_table_row.jinja2",
-            "jst/progress_bar.jinja2",
-            "jst/receipt_list_table_simple.jinja2",
-            "jst/receipt_list_table_compensations.jinja2",
-            "jst/receipt_info.jinja2",
-            "jst/receipt_suspend_form.jinja2",
-            "jst/receipt_table.jinja2",
-            "jst/receipt_table_row.jinja2",
-            "jst/vendor_report_item_table.jinja2",
         ],
         "output_filename": "checkout_templates.js"
     }

kirppu/static_src/js/checkout/clerkloginmode.coffee
@@ -52,7 +52,7 @@ class @ClerkLoginMode extends CheckoutMode
       "%s Multiple receipts active").replace("%s", icon))
 
     info = $("<div>").text(gettext("Please select receipt, which you want to continue."))
-    table = $ Templates.render("receipt_list_table_simple",
+    table = $ Template.receipt_list_table_simple(
       items: receipts
     )
 

kirppu/static_src/js/checkout/compensation_receipt.coffee
@@ -41,7 +41,7 @@ class @CompensationReceipt extends CheckoutMode
 
     # TODO: Add subtotal if receipt.extras
 
-    table = Templates.render("item_report_table",
+    table = Template.item_report_table(
       caption: gettext("Compensated Items")
       items: receipt.items
       sum: receipt.total

kirppu/static_src/js/checkout/countermode.coffee
@@ -138,7 +138,7 @@ class @CounterMode extends ItemCheckoutMode
         receipt = jqXHR.responseJSON.receipt
         dialog = new Dialog()
         dialog.title.text(gettext("Continue suspended receipt?"))
-        table = Templates.render("receipt_info", receipt: receipt)
+        table = Template.receipt_info(receipt: receipt)
         dialog.body.append(table)
         dialog.addPositive().text(gettext("Continue")).click(() =>
           console.log("Continuing receipt #{receipt.id}")
@@ -366,7 +366,7 @@ class @CounterMode extends ItemCheckoutMode
 
     dialog = new Dialog()
     dialog.title.text(gettext("Suspend receipt?"))
-    form = Templates.render("receipt_suspend_form")
+    form = Template.receipt_suspend_form()
     dialog.body.append(form)
 
     dialog.addPositive().text(gettext("Suspend")).click(() =>
@@ -486,7 +486,7 @@ class BoxReservationOperation
 
     dialog = new Dialog()
     dialog.title.text(gettext("Confirm box allocation"))
-    table = Templates.render("box_sell_allocation_dialog",
+    table = Template.box_sell_allocation_dialog(
       item: box
       request: requestBox
       text:

kirppu/static_src/js/checkout/item_receipt_table.coffee
@@ -10,12 +10,12 @@ class @ItemReceiptTable
       , options
     )
 
-    @_table = Templates.render("item_receipt_table", options)
+    @_table = Template.item_receipt_table(options)
     @_table = $(@_table)
     @body = @_table.find("tbody")
 
   createRow: (index, code, name, price=null, rounded=false) ->
-    row = Templates.render("item_receipt_table_row",
+    row = Template.item_receipt_table_row(
       index: index
       code: code
       name: name
@@ -24,7 +24,7 @@ class @ItemReceiptTable
     return $(row)
 
   row: (args) ->
-    row = Templates.render("item_receipt_table_row", args)
+    row = Template.item_receipt_table_row(args)
     return $(row)
 
   render: () -> @_table

kirppu/static_src/js/checkout/printreceipttable.coffee
@@ -5,7 +5,7 @@ class @PrintReceiptTable
   @strVendor: gettext("vendor")
 
   constructor: (caption=null)->
-    @table = $ Templates.render("receipt_table",
+    @table = $ Template.receipt_table(
       caption: caption
       vendor: @constructor.strVendor
       code: @constructor.strCode
@@ -15,13 +15,13 @@ class @PrintReceiptTable
     @body = $("tbody", @table)
 
   @joinedLine: (text="") ->
-    Templates.render("receipt_table_row",
+    Template.receipt_table_row(
       joined: true
       text: text
     )
 
   @createRow: (vendor, code, name, price, rounded) ->
-    Templates.render("receipt_table_row",
+    Template.receipt_table_row(
       vendor: vendor
       code: code
       name: name

kirppu/static_src/js/checkout/vendorcompensation.coffee
@@ -71,7 +71,7 @@ class @VendorCompensation extends CheckoutMode
 
 
     if @compensableItems.length > 0
-      table = Templates.render("item_report_table",
+      table = Template.item_report_table(
         caption: gettext("Sold Items")
         items: rows
         sum: compensableSum + provisionAmount
@@ -108,7 +108,7 @@ class @VendorCompensation extends CheckoutMode
 
 
   _createProgress: (max) ->
-    @buttonForm.empty().append(Templates.render("progress_bar", {
+    @buttonForm.empty().append(Template.progress_bar({
       max: max
     }))
     @_progressMax = max

commit 80d5521fba3f9965a06377209eb99bd61526a673

Fix receipt table dom for joined lines
Regression from b5b8eb0b015421bc8b36408135d8c42604b1c47a

kirppu/static_src/jst/receipt_table.jsx
@@ -18,7 +18,7 @@ export function receipt_table({caption}) {
 
 export function receipt_table_row({joined, vendor, code, name, price, text}) {
     if (joined) {
-        return <td colSpan="4">{text}</td>
+        return <tr><td colSpan="4">{text}</td></tr>
     }
     return (
         <tr id={code}>