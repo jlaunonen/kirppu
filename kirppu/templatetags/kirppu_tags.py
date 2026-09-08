jlaunonen/kirppu

commit acb4d5fbc8cac9a6a831636c14f782c658719e8c

backend: Fix two unreachable code inspections
The inspection is false positive, but in these cases the surrounding
conditions can be improved quite safely.

kirppu/templatetags/kirppu_tags.py
@@ -3,7 +3,6 @@ import datetime
 import json
 import re
 import typing
-import warnings
 
 from django import template
 from django.conf import settings
@@ -19,10 +18,7 @@ register = template.Library()
 
 def _get_ui_text_query(context, id_):
     event = context["event"]  # type: Event
-    source_event = context.get("source_event")  # type: typing.Union[Event, RemoteEvent]
-    if source_event is None:
-        warnings.warn("Missing source_event value when getting text " + id_)
-        source_event = event
+    source_event: Event | RemoteEvent = context["source_event"]
     database = source_event.get_real_database_alias()
     return UIText.objects.using(database).filter(event=source_event)
 

kirppu/api/receipt.py
@@ -136,7 +136,7 @@ def list_transfers(request, event):
 
 
 @ajax_func('^accounts/transfer$', method="POST", overseer=True)
-def transfer_money(request, event, src_id, dst_id, amount, note, auth, commit=False):
+def transfer_money(request, event, src_id, dst_id, amount, note, auth, commit: str | None = None):
     # Session clerk is mostly just stored into the note.
     session_clerk = get_clerk(request)
     clerk = Clerk.by_code(auth, event=event)

commit 3e4eab9054b1c994762743d57b5dfcd6c9ddd01f

backend: Fix undefined source_event context argument
Revert the removal of warning, but fix one of potential paths raising
it.
Broke in acb4d5fbc8cac9a6a831636c14f782c658719e8c.

kirppu/templatetags/kirppu_tags.py
@@ -3,6 +3,7 @@ import datetime
 import json
 import re
 import typing
+import warnings
 
 from django import template
 from django.conf import settings
@@ -17,8 +18,11 @@ register = template.Library()
 
 
 def _get_ui_text_query(context, id_):
-    event = context["event"]  # type: Event
-    source_event: Event | RemoteEvent = context["source_event"]
+    event: Event = context["event"]
+    source_event: Event | RemoteEvent | None = context.get("source_event")
+    if source_event is None:
+        warnings.warn("Missing source_event value when getting text " + id_)
+        source_event = event
     database = source_event.get_real_database_alias()
     return UIText.objects.using(database).filter(event=source_event)
 

kirppu/views/mobile.py
@@ -153,6 +153,7 @@ def _login_view(request, event):
     min_length = first(field.validators, lambda v: v.code == "min_length")
     return render(request, "kirppu/vendor_status_login.html", {
         'event': event,
+        'source_event': event.get_real_event(),
         'min_length': min_length.limit_value if min_length else 0,
         'max_length': field.max_length,
         'errors': errors,